d3d4859375b8a526bc78252481892228f2711d8d
Teenaelza <teena.thaliath@gmail.com>
2021-09-19 02:21:53 +0000

    Made changes to the column name

--- db/schema/03_markers.sql
+++ db/schema/03_markers.sql
@@ -3,9 +3,9 @@ CREATE TABLE markers (
   id SERIAL PRIMARY KEY NOT NULL,
   map_id INTEGER REFERENCES maps(id) ON DELETE CASCADE,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  title VARCHAR(255) NOT NULL,
+  title VARCHAR(255),
   description TEXT,
   image_url TEXT,
   longitude DOUBLE PRECISION NOT NULL,
-  lantitude DOUBLE PRECISION NOT NULL
+  latitude DOUBLE PRECISION NOT NULL
 );
