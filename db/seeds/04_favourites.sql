6cca025c41b5f6bf02e1177fd766c30d92bf1d9b
Teenaelza <teena.thaliath@gmail.com>
2021-09-19 02:20:22 +0000

    Add seed for maps , markers ,favourites,users table

---
+++ db/seeds/04_favourites.sql
@@ -0,0 +1,4 @@
+INSERT INTO favourites (map_id,user_id) VALUES (3,1);
+INSERT INTO favourites (map_id,user_id) VALUES (4,1);
+INSERT INTO favourites (map_id,user_id) VALUES (2,1);
+INSERT INTO favourites (map_id,user_id) VALUES (4,1);
