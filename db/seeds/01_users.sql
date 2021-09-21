a95efc9475d1bb6797bc0441c8effcc32cbe48ef
Babak Shirvani <bk.shirvani@gmail.com>
2021-09-21 22:07:03 +0000

    Update users avatar URL

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,9 +1,7 @@
 -- Users table seeds here (Example)
 INSERT INTO users (user_name,avatar_url,password) VALUES
-('Adoree','https://robohash.org/solutamolestiaedolor.png?size=50x50&set=set1','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
-('Jarib','https://robohash.org/architectoindolorem.png?size=50x50&set=set1','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
-('Grete','https://robohash.org/inciduntofficiasunt.png?size=50x50&set=set1','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
-('Spense','https://robohash.org/aspernaturrerumunde.png?size=50x50&set=set1','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
-('Electra','https://robohash.org/oditnihilsed.png?size=50x50&set=set1','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
-
-
+('Adoree','https://robohash.org/solutamolestiaedolor.png','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Jarib','https://robohash.org/architectoindolorem.png','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Grete','https://robohash.org/inciduntofficiasunt.png','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Spense','https://robohash.org/aspernaturrerumunde.png','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Electra','https://robohash.org/oditnihilsed.png','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
