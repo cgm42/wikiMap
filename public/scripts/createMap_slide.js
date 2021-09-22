e88e85b7b07b95f2853c6592db2c29ac608e6184
Teenaelza <teena.thaliath@gmail.com>
2021-09-22 16:59:08 +0000

    Add DS_Store files to git ignore

---
+++ public/scripts/createMap_slide.js
@@ -0,0 +1,16 @@
+$(() => {
+  // $("#btn-map").click(function () {
+  //   $(".marker-container").hide();
+  //   $(".map-container").animate({
+  //     width: "toggle",
+  //   });
+  // });
+  // $("#btn-marker").click(function () {
+  //   $(".map-container").hide();
+  //   $(".marker-container").animate({
+  //     width: "toggle",
+  //   });
+  // });
+  // Animations init
+new WOW().init();
+});
