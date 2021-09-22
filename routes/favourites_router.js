f6d5016c065c016e4a56be1d0b8939d76faf0878
Babak Shirvani <bk.shirvani@gmail.com>
2021-09-22 23:06:15 +0000

    fixed cookie session based on login user

--- routes/favourites_router.js
+++ routes/favourites_router.js
@@ -28,10 +28,10 @@ router.get("/:favourite_id", (req, res) => {
 
 // POST /:favourite_id/delete
 router.post("/:favourite_id/delete", (req, res) => {
-  const userID = req.session.user_id;
+  const userName = req.session.username;
   favQueries.removeFavouriteById(req.params.favourite_id)
     .then(dbres => {
-      res.redirect(`/profile/${userID}`);
+      res.redirect(`/profile/${userName}`);
 
     })
 })
